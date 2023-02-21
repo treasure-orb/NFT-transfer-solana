eedbf99b4355c4447c2ead112b7210efc344054c
MhShekel <mykhailoshekel@gmail.com>
2023-02-21 19:14:57 +0300

    update

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -1,38 +1,38 @@
 import * as React from 'react';
 
 import {
-    Dialog, DialogActions, DialogContent, DialogTitle, AppBar, Box, Toolbar, Button
+  Dialog, DialogActions, DialogContent, DialogTitle, AppBar, Box, Toolbar, Button
 } from "@material-ui/core"
 import TransferForm from './Form';
 
 export default function ButtonAppBar() {
-const [open,setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-const handleOpen = () =>{
+  const handleOpen = () => {
     setOpen(true)
-}
+  }
 
-const handleClose = () =>{
+  const handleClose = () => {
     setOpen(false)
- 
-}
+
+  }
 
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" onClick={handleOpen} style={{float:"right"}} >Transfer</Button>
+          <Button color="inherit" onClick={handleOpen} style={{ float: "right" }} >Transfer</Button>
         </Toolbar>
       </AppBar>
       <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth >
-          <DialogTitle style={{textAlign:"center" }} > TRANSFER
-              </DialogTitle>
-          <DialogContent>
-<TransferForm />
-          </DialogContent>
-          <DialogActions>
-              <Button variant="contained" onClick={handleClose} >Close</Button>
-          </DialogActions>
+        <DialogTitle style={{ textAlign: "center" }} > TRANSFER
+        </DialogTitle>
+        <DialogContent>
+          <TransferForm />
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={handleClose} >Close</Button>
+        </DialogActions>
       </Dialog>
     </div>
   );
